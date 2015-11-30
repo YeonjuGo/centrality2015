546d3a661a8086d1dcd82bfd037d9cb1566b0b2a
Yeonju <yeonjugo@gmail.com>
2015-11-30 22:15:25 +0100

    fitting method macros are updated

--- fit.C
+++ fit.C
@@ -1,8 +1,22 @@
 
+#include "TH1D.h"
+#include "TH2D.h"
+#include "TCut.h"
+#include "TTree.h"
+#include "TGraphErrors.h"
+#include "TFile.h"
+#include "TLegend.h"
+#include "TLine.h"
+#include "TCanvas.h"
+#include <iostream>
+
+using namespace std;
 
 
-static double rangeMin = 400;
-static string date = "20151123";
+static double rangeMin = 200;
+static string date = "20151126";
+static bool doUPC = 0;
+static bool express = 0;
 
 double chi2(TH1* h1, TH1* h2){
 
@@ -27,6 +41,8 @@ double chi2(TH1* h1, TH1* h2){
 
 TH1* scale(string var, TTree* nt, double s = 1, TCut cut = ""){
 
+   int nbins = 100;
+
    double xmin = 0;
    double xmax = 8000;
 
@@ -40,8 +56,29 @@ TH1* scale(string var, TTree* nt, double s = 1, TCut cut = ""){
       rangeMin = 200;
    }
 
+   if(var == "hiHF"){
+      xmax = 6000;
+      rangeMin = 200;
+   }
 
-   TH1D* h = new TH1D("h",Form("h;%s [GeV];event fraction",var.data()),200,xmin,xmax);
+   if(var == "hiHFhit"){
+      xmax = 200000;
+      rangeMin = 10000;
+   }
+
+   if(var == "hiNpix"){
+      xmax = 50000;
+      rangeMin = 1500;
+   }
+
+   if(var == "hiNtracks"){
+      xmax = 4000;
+      rangeMin = 200;
+   }
+
+
+
+   TH1D* h = new TH1D("h",Form("h;%s [GeV];event fraction",var.data()),nbins,xmin,xmax);
    h->GetXaxis()->CenterTitle();
    h->GetYaxis()->CenterTitle();
 
@@ -57,38 +94,54 @@ TH1* scale(string var, TTree* nt, double s = 1, TCut cut = ""){
 }
 
 
-void fit(string var = "hiHF", TCut dataCut = ""){
+void fit(string var = "hiHFhit", TCut dataCut = "", string cutname = "", int run = 262548){
 
+   if(cutname == "") cutname = (const char*)dataCut;
    
    TH1::SetDefaultSumw2();
 
-   //   TFile* infData = TFile::Open("root://eoscms.cern.ch//eos/cms/store/group/phys_heavyions/velicanu/forest/Run2015E/HIExpressPhysics/Merged/HiForest_Streamer_run262315.root");
-   TFile* infData = TFile::Open("root://eoscms.cern.ch//eos/cms/store/group/phys_heavyions/velicanu/forest/Run2015E/HIExpressPhysics/Merged/HiForestStreamer_Run262314-262315.root");
-   TFile* infMC = TFile::Open("root://eoscms.cern.ch//eos/cms/store/cmst3/user/mverweij/jetsPbPb/Run2Prep/Hydjet_Quenched_MinBias_5020GeV_750/crab_Run2_HydjetMB/151109_130226/HiForestMerged.root");
-   TFile* infUPC = TFile::Open("root://eoscms.cern.ch//eos/cms/store/group/phys_heavyions/chflores/Foresting_RunPrep2015/STARLIGHTProd/HiForest_Starlight_Merge19112015.root");
 
-   TTree* tref = (TTree*)infData->Get("hiEvtAnalyzer/HiTree");
-   TTree* t = (TTree*)infMC->Get("hiEvtAnalyzer/HiTree");
-   TTree* tupc = (TTree*)infUPC->Get("hiEvtAnalyzer/HiTree");
+   TFile *infData, *infMC, *infUPC;
+   TTree *tref, *t, *tupc;
 
+   if(run == 262548) infData = TFile::Open("root://eoscms.cern.ch//eos/cms/store/group/phys_heavyions/velicanu/forest/Run2015E/HIExpressPhysics/Merged/HIForestMinbiasUPC_run262548.root");
+   if(run == 262315) infData =  TFile::Open("root://eoscms.cern.ch//eos/cms/store/group/phys_heavyions/velicanu/forest/Run2015E/HIExpressPhysics/Merged/HiForestStreamer_Run262314-262315.root");
+
+   infMC = TFile::Open("root://eoscms.cern.ch//eos/cms/store/cmst3/user/mverweij/jetsPbPb/Run2Prep/Hydjet_Quenched_MinBias_5020GeV_750/crab_Run2_HydjetMB/151109_130226/HiForestMerged.root");
+   
+   if(doUPC) infUPC = TFile::Open("root://eoscms.cern.ch//eos/cms/store/group/phys_heavyions/chflores/Foresting_RunPrep2015/STARLIGHTProd/HiForest_Starlight_Merge19112015.root");
+
+   tref = (TTree*)infData->Get("hiEvtAnalyzer/HiTree");
+   t = (TTree*)infMC->Get("hiEvtAnalyzer/HiTree");
+   if(doUPC) tupc = (TTree*)infUPC->Get("hiEvtAnalyzer/HiTree");
+   
    tref->AddFriend("hltanalysis/HltTree");
    tref->AddFriend("skimanalysis/HltTree");
+   tref->AddFriend("anaTrack/trackTree");
+
    t->AddFriend("hltanalysis/HltTree");
    t->AddFriend("skimanalysis/HltTree");
-   tupc->AddFriend("hltanalysis/HltTree");
-   tupc->AddFriend("skimanalysis/HltTree");
+
+   if(doUPC){
+      tupc->AddFriend("hltanalysis/HltTree");
+      tupc->AddFriend("skimanalysis/HltTree");
+   }
+
+   TH1* hupc, *href;
+
+   if(doUPC){
+   //WARNING : No trigger for UPC
+      hupc = scale(var,tupc,1.,dataCut);
+      hupc->SetLineColor(4);
+      hupc->SetName("hupc");
+   }
 
    TCut trigger("HLT_HIL1MinimumBiasHF1AND_v1");
+   if(express) trigger = "HLT_HIL1MinimumBiasHF1ANDExpress_v1";
 
-   TH1* href = scale(var,tref,1.,trigger&&dataCut);
-
-   //WARNING : No trigger for UPC
-   TH1* hupc = scale(var,tupc,1.,dataCut);
-   hupc->SetLineColor(4);
-   hupc->SetName("hupc");
+   href = scale(var,tref,1.,trigger&&dataCut);
 
    cout<<"DATA ENTRIES : "<<tref->GetEntries(trigger&&dataCut)<<endl;
-
 
    static const int N = 40;
    TH1* h[N];
@@ -98,11 +151,16 @@ void fit(string var = "hiHF", TCut dataCut = ""){
    TGraphErrors* g = new TGraphErrors(N);
 
    double variation = 0.2;
-   double scaleMin = 0.8;
+   double scaleMin = 0.75;
 
-   if(var == "hiHF"){
-      scaleMin = 0.5;
+   if(var == "hiHF" || var == "hiHFhit"){
+      scaleMin = 0.45;
    }
+
+   if(var == "hiNpix"){
+      scaleMin = 0.9;
+   }
+
 
    for(int i = 0; i < N; ++i){  
       
@@ -132,10 +190,12 @@ void fit(string var = "hiHF", TCut dataCut = ""){
 
    g->Draw("Ap");
 
-   c1->Print(Form("figure_%s_%s_%s_%s_%s.png","chi2",var.data(),(const char*)trigger,(const char*)dataCut,date.data()));
+   c1->Print(Form("figure_%s_%s_%s_%s_run%d_%s.png","chi2",var.data(),(const char*)trigger,cutname.data(),run,date.data()));
 
    TCanvas* c2 = new TCanvas("c2","c2",600,600);
    c2->SetLogy();
+
+   h[ibest]->SetMaximum(10.*h[ibest]->GetMaximum());
    h[ibest]->Draw("hist");
    href->Draw("same");
    TLine* vertical = new TLine(rangeMin, h[ibest]->GetMinimum(), rangeMin, h[ibest]->GetMaximum());
@@ -148,6 +208,7 @@ void fit(string var = "hiHF", TCut dataCut = ""){
    leg1->SetBorderSize(0);
 
    leg1->SetTextSize(0.025);
+   leg1->AddEntry(href,Form("Run %d",run),"");
    leg1->AddEntry(href,"data","p");
    leg1->AddEntry(href,trigger,"");
    leg1->AddEntry(href,dataCut,"");
@@ -157,7 +218,7 @@ void fit(string var = "hiHF", TCut dataCut = ""){
    leg1->AddEntry(h[ibest],Form("Eff+Contam : %f",eff),"");
    leg1->Draw();
 
-   c2->Print(Form("figure_%s_%s_%s_%s_%s.png","fit",var.data(),(const char*)trigger,(const char*)dataCut,date.data()));
+   c2->Print(Form("figure_%s_%s_%s_%s_run%d_%s.png","fit",var.data(),(const char*)trigger,cutname.data(),run,date.data()));
 
    TCanvas* c3 = new TCanvas("c3","c3",600,600);
    c3->SetLogx();
@@ -169,9 +230,12 @@ void fit(string var = "hiHF", TCut dataCut = ""){
    horizontal->SetLineStyle(2);
    horizontal->Draw("same");
 
-   double supc = hdiff->Integral(1,5)/hupc->Integral(1,5);
-   hupc->Scale(supc);
-   hupc->Draw("hist same");
+   double supc = 0;
+   if(doUPC){
+      supc = hdiff->Integral(1,5)/hupc->Integral(1,5);
+      hupc->Scale(supc);
+      hupc->Draw("hist same");
+   }
 
    TLegend * leg2 = new TLegend(0.4,0.6,0.95,0.9);
    leg2->SetFillStyle(1);
@@ -183,11 +247,13 @@ void fit(string var = "hiHF", TCut dataCut = ""){
    leg2->AddEntry(href,trigger,"");
    leg2->AddEntry(href,dataCut,"");
 
-   leg2->AddEntry(hupc,"Starlight","l");
-   leg2->AddEntry(hupc,Form("Vertical scaling : %f",supc),"");
+   if(doUPC){
+      leg2->AddEntry(hupc,"Starlight","l");
+      leg2->AddEntry(hupc,Form("Vertical scaling : %f",supc),"");
+   }
    leg2->Draw();
 
-   c3->Print(Form("figure_%s_%s_%s_%s_%s.png","diff",var.data(),(const char*)trigger,(const char*)dataCut,date.data()));
+   c3->Print(Form("figure_%s_%s_%s_%s_run%d_%s.png","diff",var.data(),(const char*)trigger,cutname.data(),run,date.data()));
 
 
 
--- fitAll.C
+++ fitAll.C
@@ -2,25 +2,28 @@
 
 void fitAll(){
 
-   fit("hiHF","");
-   fit("hiHF","phfCoincFilter");
-   fit("hiHF","phfCoincFilter2");
-   fit("hiHF","phfCoincFilter3");
-   //   fit("hiHF","phfCoincFilter4");
-   //   fit("hiHF","phfCoincFilter5");
+   static const int Nvar = 7;
+   string vars[Nvar] = {"hiHF","hiHFhit","hiNtracks","hiNpix","hiEB","hiEE","hiET"};
 
-   fit("hiEB","");
-   fit("hiEB","phfCoincFilter");
-   fit("hiEB","phfCoincFilter2");
-   fit("hiEB","phfCoincFilter3");
-   //   fit("hiEB","phfCoincFilter4");
-   //   fit("hiEB","phfCoincFilter5");
 
-   fit("hiET","");
-   fit("hiET","phfCoincFilter");
-   fit("hiET","phfCoincFilter2");
-   fit("hiET","phfCoincFilter3");
-   //   fit("hiET","phfCoincFilter4");
-   //   fit("hiET","phfCoincFilter5");
+   for(int i = 0; i < Nvar; ++i){
+
+      if(0){
+      fit(vars[i].data(),"abs(zVtx[0])<10","innerVtx",262548);
+      fit(vars[i].data(),"abs(zVtx[0])>10","outerVtx",262548);
+      
+      fit(vars[i].data(),"phfCoincFilter3 && abs(zVtx[0])<10","hfCoinc3_innerVtx",262548);
+      fit(vars[i].data(),"phfCoincFilter3 && abs(zVtx[0])>10","hfCoinc3_outerVtx",262548);
+
+      fit(vars[i].data(),"","",262548);
+      fit(vars[i].data(),"phfCoincFilter3 && pprimaryVertexFilter","hfCoinc3_pVtx",262548);
+      }
+
+      fit(vars[i].data(),"phfCoincFilter3","",262548);
+      fit(vars[i].data(),"","",262315);
+      fit(vars[i].data(),"phfCoincFilter3","",262315);
+            
+   }
+
 
 }
